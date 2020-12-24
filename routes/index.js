1c97040eeb634ca65d9110c18fd6c6292647541a
Damien Vesper <ldamienvesper@gmail.com>
2020-12-23 18:01:42 -0600

    and even more stuff

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require(`express`);
+const router = express.Router();
+
+router.get(`/`, (req, res, next) => res.render(`index`, { title: `Home` }));
+router.get(`/rules`, (req, res, next) => res.render(`rules`, { title: `Rules` }));
+router.get(`/staff`, (req, res, next) => res.render(`staff`, { title: `Staff` }));
+router.get(`/faq`, (req, res, next) => res.render(`faq`, { title: `FAQ` }));
+router.get(`/support`, (req, res, next) => res.render(`support`, { title: `Support` }));
+
+module.exports = router;
